4a5d562ea04aa35f87a5eeab450f83c3d0e44345
sooyun <suy0226@naver.com>
2022-11-12 22:09:51 +0900

    예외 처리(2)
    - 잘못된 요청 접근 처리
    - 그 외 에러 처리

--- src/main/java/com/endless/study/code/BoardErrorCode.java
+++ src/main/java/com/endless/study/code/BoardErrorCode.java
@@ -7,7 +7,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum BoardErrorCode {
 
-    NO_BOARD("해당되는 게시글이 없습니다.");
+    NO_BOARD("해당되는 게시글이 없습니다."),
+
+    INVALID_REQUEST("잘못된 요청입니다."),
+    INTERNAL_SERVER_ERROR("서버 내부 오류가 발생했습니다.")
+
+    ;
 
     private final String message;
 }
--- src/main/java/com/endless/study/exception/BoardExceptionHandler.java
+++ src/main/java/com/endless/study/exception/BoardExceptionHandler.java
@@ -3,11 +3,19 @@ package com.endless.study.exception;
 import com.endless.study.code.BoardErrorCode;
 import com.endless.study.dto.BoardErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.MethodNotAllowedException;
 
 import javax.servlet.http.HttpServletRequest;
+
+import static com.endless.study.code.BoardErrorCode.INTERNAL_SERVER_ERROR;
+import static com.endless.study.code.BoardErrorCode.INVALID_REQUEST;
 
 @Slf4j
 @RestControllerAdvice
@@ -15,7 +23,7 @@ public class BoardExceptionHandler {
 
     @ExceptionHandler(BoardException.class)
     @ResponseBody
-    public BoardErrorResponse handlerBoardException(BoardException e, HttpServletRequest request) {
+    public BoardErrorResponse handleBoardException(BoardException e, HttpServletRequest request) {
         log.error("url: {}, message: {}", request.getRequestURI(), e.getDetailMessage());
         return BoardErrorResponse.builder()
                 .boardErrorCode(e.getBoardErrorCode())
@@ -23,4 +31,27 @@ public class BoardExceptionHandler {
                 .build();
     }
 
+    @ExceptionHandler(value = {
+            HttpRequestMethodNotSupportedException.class,
+            MethodArgumentNotValidException.class
+    })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public BoardErrorResponse handleNoSuchMethodException(Exception e, HttpServletRequest request) {
+        log.error("url: {}, message: {}", request.getRequestURI(), e.getMessage());
+        return BoardErrorResponse.builder()
+                .boardErrorCode(INVALID_REQUEST)
+                .message(INVALID_REQUEST.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public BoardErrorResponse handleException(Exception e, HttpServletRequest request) {
+        log.error("url: {}, message: {}", request.getRequestURI(), e.getMessage());
+        return BoardErrorResponse.builder()
+                .boardErrorCode(INTERNAL_SERVER_ERROR)
+                .message(INTERNAL_SERVER_ERROR.getMessage())
+                .build();
+    }
 }
